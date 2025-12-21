03675893f53443420dd7bd0f5b2bf4239cd60983
MarczewskiMikolaj <mikolajmarczewski13@gmail.com>
2025-12-21 10:59:39 +0100

    Cloud Run deployment

--- app.py
+++ app.py
@@ -943,5 +943,8 @@ def test_summary_route(set_id):
                          total=total,
                          percentage=round(percentage, 1))
 
+import os
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=False)
+
